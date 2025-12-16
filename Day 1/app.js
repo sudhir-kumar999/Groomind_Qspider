cb8334410dc103440a2a057602ed127c2e5377e3
sudhir-kumar999 <sk2001may@gmail.com>
2025-12-16 11:20:32 +0530

    commit

--- Day 1/app.js
+++ Day 1/app.js
@@ -1,7 +1,10 @@
-for(let i = 0;i<3;i++){
-    let str=""
-    for(let j=0;j<3;j++){
-        str= str+"* "
-    }
-    console.log(str)
+for (let i = 0; i < 3; i++) {
+  let str = "";
+  for (let j = 0; j < 3; j++) {
+    str = str + "* ";
+  }
+  console.log(str);
 }
+
+process.stdout.write("hello");
+// this process.stdout.write() is used to console the output and cursor position on the same line
